0db1b0d7efc4586c5100d3507492d3d02db1b570
h4kv <vitor.sannctorum@hotmail.com>
2025-04-02 14:01:59 -0300

    feat: buble menu

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,7 +5,10 @@ import { content } from '@/constants/content';
 
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
-import { Heading1 } from 'lucide-react';
+
+import { Bold, Italic, Strikethrough, ArrowBigUp } from 'lucide-react';
+
+import { HoverTitle } from './HoverTitle';
 
 export function Editor() {
   const editor = useEditor({
@@ -29,14 +32,27 @@ export function Editor() {
     <div>
       {editor && (
         <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-          <div className="bubble-menu">
+          <div className="bg-slate-100 shadow-sm flex gap-2 px-2 py-1 items-center rounded-2xl">
             <button
-              onClick={() =>
-                editor.chain().focus().toggleHeading({ level: 1 }).run()
-              }
-              className={editor.isActive('font-bold') ? 'is-active' : ''}
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className="editor-button group"
             >
-              Bold
+              <HoverTitle title="b" />
+              <Bold size={16} />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className="editor-button group"
+            >
+              <HoverTitle title="i" />
+              <Italic size={16} />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className="editor-button group"
+            >
+              <HoverTitle title="x" aditionalIcon={<ArrowBigUp size={14} />} />
+              <Strikethrough size={16} />
             </button>
           </div>
         </BubbleMenu>
---
+++ src/components/HoverTitle.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Command } from 'lucide-react';
+
+interface props {
+  title: string;
+  aditionalIcon?: React.ReactElement;
+}
+
+export function HoverTitle({ title, aditionalIcon }: props) {
+  return (
+    <span className="absolute flex items-center gap-1 min-w-fit left-1/2 -top-10 -translate-x-1/2 opacity-0 group-hover:opacity-100 bg-gray-700 text-white text-xs px-2 py-1 rounded transition">
+      <Command size={14} />
+      <span>+</span>
+      {aditionalIcon && (
+        <>
+          {aditionalIcon} <span>+</span>
+        </>
+      )}
+      {title}
+    </span>
+  );
+}
