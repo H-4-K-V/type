8db0d902d2d2d2822b3b09553fdadd77731044c4
h4kv <vitor.sannctorum@hotmail.com>
2025-04-04 15:46:38 -0300

    fix: fixes the layout with prettier error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-1 w-0.5 p-1 text-2xl">
+    <div className='flex h-1 w-0.5 p-1 text-2xl'>
       <h1>home</h1>
     </div>
   );
